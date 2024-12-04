f5786332358173ca921b5939e63d759895913e33
Konrad Grzanek <kongra@gmail.com>
2024-12-04 16:58:06 +0100

    sansi-svc initial commit

--- db/init/setup.sql
+++ db/init/setup.sql
@@ -1,16 +1,16 @@
 drop schema if exists public cascade;
-create schema telsos;
+create schema sansi;
 
 -- create owner user with all privileges
-drop user if exists telsos_owner;
-create user telsos_owner with password '12345' login;
+drop user if exists sansi_owner;
+create user sansi_owner with password '12345' login;
 
 -- grant all necessary privileges in one step
-alter database telsos owner to telsos_owner;
-grant all privileges on schema telsos to telsos_owner;
-grant all privileges on all tables in schema telsos to telsos_owner;
-grant usage on schema telsos to telsos_owner;
-alter default privileges in schema telsos grant all on tables to telsos_owner;
+alter database sansi owner to sansi_owner;
+grant all privileges on schema sansi to sansi_owner;
+grant all privileges on all tables in schema sansi to sansi_owner;
+grant usage on schema sansi to sansi_owner;
+alter default privileges in schema sansi grant all on tables to sansi_owner;
 
 -- set search path
-alter user telsos_owner set search_path to telsos;
+alter user sansi_owner set search_path to sansi;
